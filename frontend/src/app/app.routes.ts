ee8213212313935bd701f4e6392f1dda5de4db28
muratzenkin <muratzenkin94@gmail.com>
2025-03-29 16:51:12 +0100

    feat: login register frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Route } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 
 import { LandingpageComponent } from './components/main/landingpage.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 // path '' ist noch nicht erarbeitet und daher auskommentiert.
 export const appRoutes: Route[] = [
   { path: '', component: LandingpageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
